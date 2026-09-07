4d904c7829eb7ab6d99f04a12d41d14f04ce044f
Piyush Pal <palpiyush478@gmail.com>
2026-09-07 15:23:09 +0530

    Fix JWT secret

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -25,7 +25,7 @@ router.post('/login', async(req,res)=>{
 
     }
     if(a.password==password){
-        const token = Jwt.sign({id:a._id},process.env.jWT_SECRET,{expiresIn:"30d"})
+        const token = jwt.sign({id:a._id},process.env.JWT_SECRET,{expiresIn:"30d"})
         return res.json({
             msg:"Login Successfully",
             id:a._id,
